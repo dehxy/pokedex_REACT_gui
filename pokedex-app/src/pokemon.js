aba2a623a7a68374a76831a9a309b279d5aca4b1
dehxy <stuartforrester@live.co.uk>
2025-07-08 09:11:13 +0100

    V 0.3
    Removed
    .pokemon-card .type and replaced it with a colourised weakness catalogue.

--- pokedex-app/src/pokemon.js
+++ pokedex-app/src/pokemon.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const Pokemon = ({ pokemon }) => {
   return (
     <div className="pokemon-card">
@@ -9,10 +8,23 @@ const Pokemon = ({ pokemon }) => {
       <p>Number: {pokemon.num}</p>
       <div className="types">
         {pokemon.type.map(type => (
-          <span key={type} className="type">{type}</span>
+          //new class here to style the types, converting to lowercase for a consistent class name
+          <span key={type} className={`type type-${type.toLowerCase()}`}>{type}</span>
         ))}
       </div>
-      <p>Weaknesses: {pokemon.weaknesses.join(', ')}</p>
+
+        <div className="weaknesses">
+        <p>Weaknesses:</p>
+        <div className="weakness-list">
+          {
+            // loops through each weakness just like pokemon types.
+            // Each weakness should get its own <span> with a class based on its type.
+            pokemon.weaknesses.map(weakness => (
+              <span key={weakness} className={`type type-${weakness.toLowerCase()}`}>{weakness}</span>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 };
